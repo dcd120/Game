9dab8181b80108f6f75f81a92582811af6aa0f82
dcd120 <dcd120@mail.ru>
2018-08-25 17:23:55 +1000

    урок 2

---
+++ myGame/myGame/Asteroid.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+
+namespace MyGame
+{
+    class Asteroid : BaseObject, ICloneable
+    {
+        public int Power { get; set; }
+        protected Bitmap image;
+
+        public Asteroid(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+            image = new Bitmap("aster.png");
+            Power = 1;
+        }
+        public override void Draw()
+        {
+            //Game.Buffer.Graphics.FillEllipse(Brushes.White, Pos.X, Pos.Y, Size.Width, Size.Height);
+            Game.Buffer.Graphics.DrawImage(image, Pos.X, Pos.Y, Size.Width, Size.Height);
+        }
+        public override void Update()
+        {
+            Pos.X = Pos.X + Dir.X;
+            //Pos.Y = Pos.Y + Dir.Y;
+            if (Pos.X < -Size.Width)
+            {
+                Ressurect();
+            }
+            //if (Pos.X > Game.Width) Dir.X = -Dir.X;
+            //if (Pos.Y < 0) Dir.Y = -Dir.Y;
+            //if (Pos.Y > Game.Height) Dir.Y = -Dir.Y;
+        }
+
+
+        // пример реализации интерфейса копирования для астероида
+        public object Clone()
+        {
+            // Создаем копию нашего астероида
+            Asteroid asteroid = new Asteroid(new Point(Pos.X, Pos.Y), new
+            Point(Dir.X, Dir.Y), new Size(Size.Width, Size.Height));
+            // Не забываем скопировать новому астероиду Power нашего астероида
+            asteroid.Power = Power;
+            return asteroid;
+        }
+
+        public override void Ressurect()
+        {
+            Pos.X = Game.Width + 100;
+            Pos.Y = Game.rnd.Next(0, Game.Height);
+            int nSize = Game.rnd.Next(5, 50);
+            Size.Height = nSize;
+            Size.Width = nSize;
+            Dir.X = -nSize / 5;
+        }
+    }
+
+}
---
+++ myGame/myGame/Bullet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+
+namespace MyGame
+{
+    class Bullet : BaseObject
+    {
+        protected Bitmap image;
+
+        public Bullet(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+            image = new Bitmap("bullet.png");
+            //image.RotateFlip(RotateFlipType)
+        }
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.DrawImage(image, Pos.X, Pos.Y, Size.Width, Size.Height);
+            //Game.Buffer.Graphics.DrawRectangle(Pens.Green, Pos.X, Pos.Y, Size.Width, Size.Height);
+            //Game.Buffer.Graphics.DrawLine(Pens.OrangeRed, Pos.X - 5, Pos.Y + Size.Height / 2, Pos.X, Pos.Y + Size.Height / 2);
+
+        }
+
+        public override void Ressurect()
+        {
+            Pos.X = 10;
+            Pos.Y = Game.rnd.Next(10, Game.Height - 10);
+        }
+
+        public override void Update()
+        {
+            Pos.X = Pos.X + Dir.X;
+            if (Pos.X > Game.Width)
+            {
+                Ressurect();
+            }
+        }
+    }
+}
